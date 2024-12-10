470814d4d9087a00f53ce399d0c353e3e247a81c
zakaria <zakariaabourida980@gmail.com>
2024-12-10 19:33:38 +0100

    Update ValidationController.php

--- app/Http/Controllers/API/ValidationController.php
+++ app/Http/Controllers/API/ValidationController.php
@@ -16,21 +16,21 @@ class ValidationController extends Controller
     }
     public function validation(Request $request)
     {
-        
-        $blocage = $this->validationService->validation($request);
-        return response()->json(['Validation' => $blocage], 200);
+
+        $validation = $this->validationService->validation($request);
+        return response()->json(['validation' => $validation], 200);
     }
-       public function updateValidation(Request $request)
+    public function updateValidation(Request $request)
     {
-        
-        $blocage = $this->validationService->updateValidationApi($request);
-        return response()->json(['Validation' => $blocage], 200);
+
+        $validation = $this->validationService->updateValidationApi($request);
+        return response()->json(['validation' => $validation], 200);
     }
-    
-    
+
+
     public function getValidation($id)
     {
-        
+
         $validation = $this->validationService->getValidationApi($id);
         return response()->json(['Validation' => $validation], 200);
     }
