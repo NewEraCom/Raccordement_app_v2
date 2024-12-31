a366f9e24b9a3ed6ae05e07abb495362d7c7acde
zakaria <zakariaabourida980@gmail.com>
2024-12-31 12:55:10 +0100

    Update NotificationController.php

--- app/Http/Controllers/API/NotificationController.php
+++ app/Http/Controllers/API/NotificationController.php
@@ -13,7 +13,11 @@ class NotificationController extends Controller
 {
     public function index($id)
     {
+        $tenDaysAgo = now()->subDays(10);
+
+        // Fetch notifications from the last 10 days
         $notifications = Notification::where('user_id', $id)
+            ->where('created_at', '>=', $tenDaysAgo) // Filter notifications from the last 10 days
             ->with(['affectation.client']) // Eager load affectation and its related client
             ->orderBy('created_at', 'desc')
             ->paginate(10); // 10 items per page
