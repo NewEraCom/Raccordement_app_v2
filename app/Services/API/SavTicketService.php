f0731e257cac3d43557a976a16d2551c5d5ec122
zakaria <zakariaabourida980@gmail.com>
2025-01-10 12:32:28 +0100

    Update SavTicketService.php

--- app/Services/API/SavTicketService.php
+++ app/Services/API/SavTicketService.php
@@ -121,16 +121,16 @@ class SavTicketService
     public function storeImageBlocageSav(Request $request)
     {
         // Validate the incoming request
-        $validated = $request->validate([
-            'image' => 'nullable|string|max:255', // Optional description
-            'attachement' => 'required|file|mimes:jpg,jpeg,png,pdf|max:2048', // Validate file type and size
-            'blocage_sav_id' => 'required|integer|exists:blocage_savs,id', // Ensure blocage_sav_id exists
-        ]);
+        // $validated = $request->validate([
+        //     'image' => 'nullable|string|max:255', // Optional description
+        //     'image_file' => 'required|file|mimes:jpg,jpeg,png,pdf|max:2048', // Validate file type and size
+        //     'blocage_sav_id' => 'required|integer|exists:blocage_savs,id', // Ensure blocage_sav_id exists
+        // ]);
 
         // Handle file upload
         $filePath = null;
-        if ($request->hasFile('attachement')) {
-            $filePath = $request->file('attachement')->store('uploads', 'public');
+        if ($request->hasFile('image_file')) {
+            $filePath = $request->file('image_file')->store('uploads', 'public');
         }
 
         // Create the record in the database
