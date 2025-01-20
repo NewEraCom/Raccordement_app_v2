d2d49d311c12e0e5ed0bca5eee09ce5ae956103b
EL JAOUANI <133882453+ELJAOUANI@users.noreply.github.com>
2025-01-20 12:27:08 +0100

    add cause relance to client_savt table

--- database/migrations/2025_01_20_120527_add_cause_to_sav_client_table.php
+++ database/migrations/2025_01_20_120527_add_cause_to_sav_client_table.php
@@ -14,7 +14,7 @@ class AddCauseToSavClientTable extends Migration
     public function up()
     {
         Schema::table('sav_client', function (Blueprint $table) {
-            //
+            $table->string('cause')->nullable()->after('status'); 
         });
     }
 
@@ -26,7 +26,7 @@ class AddCauseToSavClientTable extends Migration
     public function down()
     {
         Schema::table('sav_client', function (Blueprint $table) {
-            //
+            $table->dropColumn('cause'); 
         });
     }
 }
